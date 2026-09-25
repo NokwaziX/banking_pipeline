19d2b703d82e5b154f481707b479abd639d943ea
NokwaziX <nokwazi.mahlangu@gmail.com>
2026-09-26 00:07:02 +0200

    Add verification code: WTC-JZQR6V3F

--- src/clean.py
+++ src/clean.py
@@ -58,3 +58,5 @@ if __name__ == "__main__":
     missing_values_total = check_missing_values(transactions)
     print("Missing values per column:")
     print(missing_values_total)
+
+    #SOMETHING MISSING HERE
